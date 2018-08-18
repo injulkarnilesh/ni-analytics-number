b6397b0df31be18b70de405b15656e67855c6789
Injulkar, Nilesh (DS) <Nilesh.Injulkar@cdk.com>
2018-08-18 12:16:36 +0530

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NiAnalyticsNumberModule } from './analytics-number/ni-analytics-number.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NiAnalyticsNumberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
