7db9884e27cb9cabe6f885c677276b2b7cbe06ec
Injulkar, Nilesh (DS) <Nilesh.Injulkar@cdk.com>
2018-08-29 17:13:51 +0530

    Color service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
+import { NiAnalyticsNumberComponent } from './analytics-number/components/ni-analytics-number.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ColorServiceBuilder } from './analytics-number/services/color-service.builder';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NiAnalyticsNumberComponent
       ],
+      providers: [
+        ColorServiceBuilder
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +19,11 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('ni-analytics-number');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ni-analytics-number!');
-  }));
+
 });
