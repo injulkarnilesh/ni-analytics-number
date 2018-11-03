319dea8772baaf7c88e1df80667a04a37f6c1e58
Injulkar, Nilesh (DS) <injulkarnilesh@gmail.com>
2018-11-03 10:40:38 +0530

    Font size support

--- src/app/analytics-number/components/ni-analytics-number.component.ts
+++ src/app/analytics-number/components/ni-analytics-number.component.ts
@@ -12,13 +12,20 @@ import { StyleService } from '../services/style-service';
 export class NiAnalyticsNumberComponent implements OnInit, OnChanges {
 
     @Input() value: number;
-    @Input() displayValue: string;
+    @Input() displayValue: string;  // TODO USE
     @Input() fromValue: number;
     @Input() toValue: number;
 
+    @Input() fontUnit: string;
+    @Input() fromFontSize: number;
+    @Input() toFontSize: number;
+
     color: string;
+    fontSize: string;
 
     rangePostionBuilder: RangePositionBuilder;
+
+    fontUnitsSupported: string[] = ['px', 'em', '%', 'pt' ];
 
     constructor(private rangePositionBuilderFactory: RangePositionBuilderFactory,
                 private styleService: StyleService) {
@@ -31,17 +38,42 @@ export class NiAnalyticsNumberComponent implements OnInit, OnChanges {
             return;
         }
         this.rangePostionBuilder = this.rangePositionBuilderFactory.forBaseRange(Range.between(this.fromValue, this.toValue));
-        this.color = this.styleService.getColor(this.getRangePositionCalculator(this.value));
+        this.setColor();
+        this.setFont();
     }
 
     ngOnChanges(changes: SimpleChanges) {
         if (this.rangePostionBuilder && changes.value) {
-            this.color = this.styleService.getColor(this.getRangePositionCalculator(changes.value.currentValue));
+            this.setColor();
+            this.setFont();
         }
     }
 
     private getRangePositionCalculator(value: number): RangePositionCalculator {
         return this.rangePostionBuilder.withValue(value);
     }
+
+    private setColor(): void {
+        this.color = this.styleService.getColor(this.getRangePositionCalculator(this.value));
+    }
+
+    private setFont(): void {
+        console.log('Setting font size to', this.fromFontSize, this.toFontSize, this.fontUnit);
+        if (this.validFontSize() && this.validFontUnit()) {
+            this.fontSize = this.getRangePositionCalculator(this.value)
+                                .getPositionBetween(Range.between(this.fromFontSize, this.toFontSize)) +
+                                this.fontUnit;
+            console.log('Set font size to', this.fontSize);
+        }
+    }
+
+    private validFontSize(): boolean {
+        return this.fromFontSize != null && this.toFontSize != null;
+    }
+
+    private validFontUnit(): boolean {
+        return this.fontUnit && this.fontUnitsSupported.includes(this.fontUnit.toLocaleLowerCase());
+    }
+
 
 }
